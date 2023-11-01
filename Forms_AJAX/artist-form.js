1fe53ec143cde4e650fe8e14cf2225e05852d9b4
jleon9 <jean-francois.leon@mail.mcgill.ca>
2023-11-01 00:13:22 -0400

    Assignment 3 Finished

--- Forms_AJAX/artist-form.js
+++ Forms_AJAX/artist-form.js
@@ -1,23 +1,18 @@
-//const searchResult = document.getElementById("searchResult");
-const artistName = document.getElementById('userInput');
 document.getElementById('artistForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault(); // Prevent form submission to keep seing user input in search bar
 
-    const artistName = document.getElementById('userInput').value;
-    
+    const userInput = document.getElementById('userInput').value; // Fetch the user input
 
     // Fetch artist releases and display in the table
-    fetchArtistReleases(artistName)
-    .then(releases => {
-        if (releases) {
-        console.log('Releases for ' + artistName + ':', releases);
-        //displayReleasesTable(releases);
-        }
-        
-    });
+    fetchArtistReleases(userInput)
+        .then(releases => {
+            if (releases) {
+                displayReleasesTable(releases, userInput); // Pass userInput to display function
+            }
+        });
 });
 
-async function fetchArtistReleases() {
+async function fetchArtistReleases(artistName) {
     try {
         const response = await fetch(`https://musicbrainz.org/ws/2/release/?query=artist:${artistName}&fmt=json`);
         const data = await response.json();
@@ -28,28 +23,21 @@ async function fetchArtistReleases() {
     }
 }
 
-
 function displayReleasesTable(releases) {
     const tableBody = document.getElementById('tableData');
+    tableBody.innerHTML = ''; // Clear previous data
 
     releases.forEach(release => {
-        if(release.name === artistName) {
-            const row = document.createElement('tr');
+        const row = document.createElement('tr');
 
-            const titleCell = document.createElement('td');
-            titleCell.textContent = release.title;
-            row.appendChild(titleCell);
+        const titleCell = document.createElement('td');
+        titleCell.textContent = release.title;
+        row.appendChild(titleCell);
 
-            const dateCell = document.createElement('td');
-            dateCell.textContent = release.date ? release.date : 'N/A';
-            row.appendChild(dateCell);
+        const dateCell = document.createElement('td');
+        dateCell.textContent = release.date ? release.date : 'N/A';
+        row.appendChild(dateCell);
 
-            tableBody.appendChild(row);
-        }
-        
+        tableBody.appendChild(row);
     });
 }
-
-
-    
-
