ca376c3e712594da4859b5d70d40729eef467425
jleon9 <jean-francois.leon@mail.mcgill.ca>
2023-11-03 01:04:08 -0400

    Used AJAX for the forms. Dates need to be sorted and filtered.

--- Forms_AJAX/artist-form.js
+++ Forms_AJAX/artist-form.js
@@ -1,41 +1,63 @@
 document.getElementById('artistForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission to keep seing user input in search bar
-
-    const userInput = document.getElementById('userInput').value; // Fetch the user input
-
-    // Fetch artist releases and display in the table
-    fetchArtistReleases(userInput)
-        .then(releases => {
-            if (releases) {
-                displayReleasesTable(releases, userInput); // Pass userInput to display function
-            }
-        });
+    event.preventDefault(); // Prevent form submission
+    const userInput = document.getElementById('userInput').value; // Get user input
+    fetchReleaseGroups(userInput);
 });
 
-async function fetchArtistReleases(artistName) {
-    try {
-        const response = await fetch(`https://musicbrainz.org/ws/2/release/?query=artist:${artistName}&fmt=json`);
-        const data = await response.json();
-        return data.releases; // Returns array of releases
-    } catch (error) {
-        console.error('Error fetching artist releases:', error);
-        return null;
-    }
+function fetchReleaseGroups(artistName) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                filterAndDisplayReleaseGroups(data['release-groups']);
+            } else {
+                console.error('Error fetching release groups:', xhr.status);
+            }
+        }
+    };
+    xhr.open('GET', `https://musicbrainz.org/ws/2/release-group/?query=artist:${artistName}&fmt=json`);
+    xhr.send();
 }
 
-function displayReleasesTable(releases) {
+function filterAndDisplayReleaseGroups(releaseGroups) {
+    const uniqueReleases = {};
+
+    releaseGroups.forEach(release => {
+        const releaseDate = release['first-release-date'];
+        if (!(release.title in uniqueReleases) || (uniqueReleases[release.title] > releaseDate) && release.date)  {
+            uniqueReleases[release.title] = releaseDate;
+        }
+    });
+
+    const sortedReleases = Object.keys(uniqueReleases)
+    .sort((a, b) => {
+            if (uniqueReleases[a] < uniqueReleases[b] || uniqueReleases[a] === 'N/A') {
+                return -1;
+            } else if (uniqueReleases[a] > uniqueReleases[b] || uniqueReleases[b] === 'N/A') {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+    console.log(sortedReleases);
+    displayReleaseGroups(sortedReleases, uniqueReleases);
+}
+
+function displayReleaseGroups(releases, uniqueReleases) {
     const tableBody = document.getElementById('tableData');
-    tableBody.innerHTML = ''; // Clear previous data
+    tableBody.innerHTML = '';
 
     releases.forEach(release => {
         const row = document.createElement('tr');
 
         const titleCell = document.createElement('td');
-        titleCell.textContent = release.title;
+        titleCell.textContent = release;
         row.appendChild(titleCell);
 
         const dateCell = document.createElement('td');
-        dateCell.textContent = release.date ? release.date : 'N/A';
+        dateCell.textContent = uniqueReleases[release];
         row.appendChild(dateCell);
 
         tableBody.appendChild(row);
